652ed3386c37f125e288320c754294cf99b036c6
Ryan Torant <pachesantiago@gmail.com>
2018-10-06 15:49:09 -0300

    Fixed some sintax errors in SRegistry.h

--- src/Serialization/SRegistry.h
+++ src/Serialization/SRegistry.h
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include "SIndividual.h"
 
+// Need to define it first so that the map inside SRegistry sees the hash instanciation first
 namespace agio
 {
 	struct SComponent
@@ -12,7 +13,7 @@ namespace agio
 		int GroupID;
 		int ComponentID;
 
-		bool operator==(const ComponentRef & other) const
+		bool operator==(const SComponent & other) const
 		{
 			return GroupID == other.GroupID &&
 				ComponentID == other.ComponentID;
@@ -20,21 +21,6 @@ namespace agio
 	};
 
 	typedef std::vector<SComponent> SMorphologyTag;
-
-	struct SRegistry
-	{
-		std::unordered_map<SMorphologyTag, vector<SIndividual>> Species;
-
-		
-		void LoadFromFile(const string& Path) 
-		{ 
-			/* TODO : Implement! 
-				for record in StagnantSpecies
-					org = SIndividual(record.HistoricalBestGenome)
-					species[record.Morphology].push_back(org)
-			*/
-		};
-	};
 }
 
 // This needs to be outside of the agio namespace
@@ -48,13 +34,33 @@ namespace std
 			// Ref : [https://stackoverflow.com/questions/20511347/a-good-hash-function-for-a-vector]
 			std::size_t seed = k.size();
 
-			for (auto[gid, cid] : k)
+			//for (auto[gid, cid] : k)
+			for (auto data : k) // No C++ 17 on UE4 :(
 			{
-				seed ^= gid + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-				seed ^= cid + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+				seed ^= data.GroupID + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+				seed ^= data.ComponentID + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 			}
 
 			return seed;
 		}
 	};
 }
+
+namespace agio
+{
+	struct SRegistry
+	{
+		std::unordered_map<SMorphologyTag, std::vector<SIndividual>> Species;
+
+		
+		void LoadFromFile(const std::string& Path) 
+		{ 
+			/* TODO : Implement! 
+				for record in StagnantSpecies
+					org = SIndividual(record.HistoricalBestGenome)
+					species[record.Morphology].push_back(org)
+			*/
+		};
+	};
+}
+
