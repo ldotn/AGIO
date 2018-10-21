5ca0dd5a5f494ae6018f45060416cd85685b56bb
Ryan <Ryan@DESKTOP-DK20VUS>
2018-10-21 17:25:41 -0300

    Added missing files

---
+++ src/Tests/PreyPredator/PreyPredator.h
@@ -0,0 +1,19 @@
+#pragma once
+
+extern int WorldSizeX;
+extern int WorldSizeY;
+extern float FoodScoreGain;
+extern float KillScoreGain;
+extern float DeathPenalty;
+extern int FoodCellCount;
+extern int MaxSimulationSteps;
+extern int SimulationSize;
+extern int PopSizeMultiplier;
+extern int PopulationSize;
+extern int GenerationsCount;
+extern float LifeLostPerTurn;
+extern float BorderPenalty;
+extern float WastedActionPenalty;
+
+void runEvolution();
+void runSimulation();
