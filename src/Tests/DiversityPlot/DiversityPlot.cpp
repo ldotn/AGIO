538b387c3b60482b020d36d02325d17b98bb6320
Nicolas Ottonello <nicolas.ottonello96@gmail.com>
2018-11-04 22:50:54 -0300

    SFML Working! Need to clean up cmake, also need to check if it works on Windows

--- src/Tests/DiversityPlot/DiversityPlot.cpp
+++ src/Tests/DiversityPlot/DiversityPlot.cpp
@@ -5,10 +5,5 @@
 namespace plt = matplotlibcpp;
 
 void runSimulation() {
-    while (true) {
-        plt::clf();
-        plt::plot(1, 1, "ob");
-        plt::plot(3, 3, "ob");
-        plt::pause(0.1);
-    }
+
 }
